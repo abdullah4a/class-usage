c4133b94d7b6bb9f641cdf157de333053a13886f
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-16 00:06:37 +0500

    Crated a new APP
    push to main

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # class-usage
+This app is created by Abdullah Iqbal
+Software Engg. From Comsats University
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div>
+      <button @click="onclick">click me to input</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class HelloWorld extends Vue {
+  letsgo = new go();
+}
+export class go {
+  goname: string;
+  consteuctor() {
+    this.goname = "Abdullah Iqbal";
+  }
+  OnClicke(): void {
+    console.log(this.goname);
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
