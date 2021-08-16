47b4dcdc1571af7b08d576a13de9b79739e876f6
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-08-16 10:13:52 +0500

    Some Changes

---
+++ src/components/comp1.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { mixins } from "vue-class-component";
+import { Component, Prop, Vue } from "vue-property-decorator";
+// import { VNodeDirective } from "vue/types/umd";
+const GreetingProps = Vue.extend({
+  props: {
+    name: String,
+  },
+});
+
+// Use `mixins` helper to combine defined props and a mixin.
+@Component
+export default class Greeting extends mixins(GreetingProps) {
+  get message(): string {
+    this.name = this.hello();
+    // this.name will be typed
+    return "Hello, " + this.name;
+  }
+
+  hello(): string {
+    return "Bilal";
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
